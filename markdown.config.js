d0cba5d3453546f3b9e42ba8bb73d58daed94659
Dustin Schau <dustinschau@gmail.com>
2019-01-25 15:15:31 -0600

    chore: add labs to base readme

--- markdown.config.js
+++ markdown.config.js
@@ -1,8 +1,15 @@
 const path = require('path')
+const fs = require('fs')
 const labTemplate = require(`./labs/readme-template`)
 
 module.exports = {
   transforms: {
+    ALL_LABS() {
+      const base = path.resolve('labs')
+      const labs = fs.readdirSync(base)
+        .filter(fileOrDirectory => fs.statSync(path.join(base, fileOrDirectory)).isDirectory())
+      return labs.map(lab => `- [${lab}](./${path.join('labs', lab)})`).join('\n')
+    },
     LAB(content, options, { originalPath }) {
       const relativePath = path.relative(path.resolve('labs'), originalPath)
       const lab = path.dirname(relativePath)
